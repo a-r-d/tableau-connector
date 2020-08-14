5e79be78a6f70863b8dea645876bbfdb62b088b3
Aaron Decker <aaron@ard.ninja>
2020-08-14 17:07:36 -0500

    Update arangowdc.js

--- assets/arangowdc.js
+++ assets/arangowdc.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   "use strict";
   /**@type {HTMLDivElement} */
   var queriesDiv = document.getElementById("queries");
@@ -12,7 +12,7 @@
   var queries = [];
 
   function extend(base, ext) {
-    Object.keys(ext).forEach(function(key) {
+    Object.keys(ext).forEach(function (key) {
       base[key] = ext[key];
     });
     return base;
@@ -54,7 +54,7 @@
     if (typeof result === "number") return tableau.dataTypeEnum.float;
     if (Array.isArray(result)) return result.map(inferSchema);
     if (result && typeof result === "object") {
-      return Object.keys(result).reduce(function(obj, key) {
+      return Object.keys(result).reduce(function (obj, key) {
         obj[key] = inferSchema(result[key]);
         return obj;
       }, {});
@@ -88,15 +88,6 @@
     }
   }
 
-  addQueryBtn.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    addQuery();
-  });
-  form.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    submit();
-  });
-
   var connector = tableau.makeConnector();
   connector.init = function init(done) {
     if (tableau.connectionData) {
@@ -120,7 +111,7 @@
   };
   connector.getSchema = function getSchema(done) {
     done(
-      queries.map(function(query) {
+      queries.map(function (query) {
         return {
           id: query.id,
           alias: query.alias,
@@ -128,21 +119,21 @@
           columns: [].concat.apply(
             [{ id: "_i", alias: "index", dataType: tableau.dataTypeEnum.int }],
             Array.isArray(query.schema)
-              ? query.schema.map(function(schema, i) {
+              ? query.schema.map(function (schema, i) {
                   return {
                     id: "value" + i,
-                    dataType: schema
+                    dataType: schema,
                   };
                 })
               : typeof query.schema === "string"
               ? [{ id: "value", dataType: query.schema }]
-              : Object.keys(query.schema).map(function(key) {
+              : Object.keys(query.schema).map(function (key) {
                   return {
                     id: key,
-                    dataType: query.schema[key]
+                    dataType: query.schema[key],
                   };
                 })
-          )
+          ),
         };
       })
     );
@@ -153,7 +144,7 @@
       "authorization",
       "Basic " + btoa(tableau.username + ":" + tableau.password)
     );
-    var i = findIndex(queries, function(query) {
+    var i = findIndex(queries, function (query) {
       return query.id === table.tableInfo.id;
     });
     var query = queries[i].query;
@@ -169,25 +160,25 @@
       method: "POST",
       body: JSON.stringify({
         query: query,
-        bindVars: incremental ? { OFFSET: OFFSET } : undefined
-      })
+        bindVars: incremental ? { OFFSET: OFFSET } : undefined,
+      }),
     })
-      .then(function(response) {
-        return response.json().then(function(body) {
+      .then(function (response) {
+        return response.json().then(function (body) {
           response._body = body;
           return response;
         });
       })
-      .then(function(response) {
+      .then(function (response) {
         if (!response.ok) {
           throw new Error(response._body.errorMessage);
         }
         table.appendRows(
-          response._body.result.map(function(row, i) {
+          response._body.result.map(function (row, i) {
             return extend(
               { _i: OFFSET + i },
               Array.isArray(schema)
-                ? row.reduce(function(obj, value, i) {
+                ? row.reduce(function (obj, value, i) {
                     obj["value" + i] = value;
                     return obj;
                   }, {})
@@ -205,21 +196,21 @@
           console.log("Fetching more results ...");
           return fetch("../_api/cursor/" + cursorId, {
             headers: headers,
-            method: "PUT"
+            method: "PUT",
           })
-            .then(function(response) {
-              return response.json().then(function(body) {
+            .then(function (response) {
+              return response.json().then(function (body) {
                 response._body = body;
                 return response;
               });
             })
-            .then(function(response) {
+            .then(function (response) {
               table.appendRows(
-                response._body.result.map(function(row, i) {
+                response._body.result.map(function (row, i) {
                   return extend(
                     { _id: offset + i },
                     Array.isArray(schema)
-                      ? row.reduce(function(obj, value, j) {
+                      ? row.reduce(function (obj, value, j) {
                           obj["value" + j] = value;
                           return obj;
                         }, {})
@@ -236,27 +227,25 @@
         }
         return Promise.resolve().then(fetchMore);
       })
-      .catch(function(error) {
-        console.error('Error: ', error)
+      .catch(function (error) {
+        console.error("Error: ", error);
         alert(error.message);
       })
       .finally(done);
   };
-
-  tableau.registerConnector(connector);
 
   function submit() {
     tableau.username = form.username.value;
     tableau.password = form.password.value;
     connectionName = form.dsn.value || "ArangoDB WDC";
     queries = [];
-    var queryAql = splat(form.queryAql).map(function(el) {
+    var queryAql = splat(form.queryAql).map(function (el) {
       return el.value;
     });
-    var queryId = splat(form.queryId).map(function(el) {
+    var queryId = splat(form.queryId).map(function (el) {
       return el.value;
     });
-    var queryAlias = splat(form.queryAlias).map(function(el) {
+    var queryAlias = splat(form.queryAlias).map(function (el) {
       return el.value;
     });
     var headers = new Headers();
@@ -265,25 +254,25 @@
       "Basic " + btoa(tableau.username + ":" + tableau.password)
     );
     return fetch("../_api/version", { headers: headers })
-      .then(function(response) {
+      .then(function (response) {
         if (!response.ok) {
           throw new Error("Invalid credentials");
         }
-        return queryAql.reduce(function(p, _, i) {
-          return p.then(function() {
+        return queryAql.reduce(function (p, _, i) {
+          return p.then(function () {
             console.log("Parsing query #" + (i + 1) + " ...");
             return fetch("../_api/query", {
               headers: headers,
               method: "POST",
-              body: JSON.stringify({ query: queryAql[i] })
+              body: JSON.stringify({ query: queryAql[i] }),
             })
-              .then(function(response) {
-                return response.json().then(function(body) {
+              .then(function (response) {
+                return response.json().then(function (body) {
                   response._body = body;
                   return response;
                 });
               })
-              .then(function(response) {
+              .then(function (response) {
                 if (!response.ok) {
                   throw new Error(
                     "Invalid query #" +
@@ -309,15 +298,15 @@
                   id: queryId[i],
                   alias: queryAlias[i],
                   query: queryAql[i],
-                  incremental: response._body.bindVars[0] === "OFFSET"
+                  incremental: response._body.bindVars[0] === "OFFSET",
                 });
               });
           });
         }, Promise.resolve());
       })
-      .then(function() {
-        return queries.reduce(function(p, _, i) {
-          return p.then(function() {
+      .then(function () {
+        return queries.reduce(function (p, _, i) {
+          return p.then(function () {
             console.log("Inferring schema for query #" + i + " ...");
             var query = queries[i].query;
             var incremental = queries[i].incremental;
@@ -327,39 +316,50 @@
               body: JSON.stringify({
                 query: query,
                 bindVars: incremental ? { OFFSET: 0 } : undefined,
-                batchSize: 1
-              })
+                batchSize: 1,
+              }),
             })
-              .then(function(response) {
-                return response.json().then(function(body) {
+              .then(function (response) {
+                return response.json().then(function (body) {
                   response._body = body;
                   return response;
                 });
               })
-              .then(function(response) {
+              .then(function (response) {
                 queries[i].schema = inferSchema(response._body.result[0]);
                 if (response._body.id) {
                   console.log("Disposing of cursor ...");
                   return fetch("../_api/cursor/" + response._body.id, {
                     headers: headers,
-                    method: "DELETE"
+                    method: "DELETE",
                   });
                 }
               });
           });
         }, Promise.resolve());
       })
-      .then(function() {
+      .then(function () {
         tableau.connectionData = JSON.stringify({
           connectionName: connectionName,
-          queries: queries
+          queries: queries,
         });
         tableau.connectionName = connectionName;
+        console.log("Calling tableau submit");
         tableau.submit();
       })
-      .catch(function(error) {
-        console.error('Error: ', error)
+      .catch(function (error) {
+        console.error("Error submitting: ", error);
         errorsDiv.innerHTML = error.message;
       });
   }
+
+  addQueryBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    addQuery();
+  });
+  form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    submit();
+  });
+  tableau.registerConnector(connector);
 })();
